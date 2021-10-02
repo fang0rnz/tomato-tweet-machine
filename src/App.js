45ba19229a77f8a5f2d7af945116d0e5037b3f01
Lucas Sartori <lucas.sartorimoraes@avast.com>
2021-10-02 21:59:53 +0200

    solved problems

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import "./styles.css";
-import React, { useState, useEffect } from "react";
-
-import tomato from "./tomato.png";
+import React, { useState, useEffect, useRef } from "react";
 
 import { processTweet } from "./marketingLogic";
 import { useDebounce } from "./utils/stateHooks";
+import TextArea from "./components/TextArea/TextArea";
 
 const bigtweet =
-  "Lorem #tomato dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim";
-
+  "Lorem dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim #tomato";
 export default function App() {
   const [tweet, setTweet] = useState(bigtweet);
   const [processedTweet, setProcessedTweet] = useState(tweet);
+  const outputRef = useRef();
 
   const debouncedTweet = useDebounce(tweet, 500);
 
@@ -29,16 +28,23 @@ export default function App() {
 
   return (
     <div className="App">
-      <marquee>
-        <img className="tomato" src={tomato} alt="drawing of a tomato" />
-        Welcome to Tomato Tweet Machine!
-        <img className="tomato" src={tomato} alt="drawing of a tomato" />
-      </marquee>
-      Text to tweetify:
-      <textarea id="input" value={tweet} onChange={onChange}></textarea>
-      <br />
-      Tweetified text:
-      <div id="output">{processedTweet}</div>
+      <div className="container">
+        <div className="subTitle">Text to tweetify:</div>
+        <TextArea tabIndex="1" id="input" value={tweet} onChange={onChange} />
+        <br />
+        <div className="subTitle"> Tweetified text:</div>
+        <div
+          ref={outputRef}
+          onFocus={() => {
+            navigator.clipboard.writeText(processedTweet);
+          }}
+          tabIndex="2"
+          id="output"
+        >
+          {processedTweet}
+        </div>
+        <span></span>
+      </div>
     </div>
   );
 }
--- src/marketingLogic.js
+++ src/marketingLogic.js
@@ -1,17 +1,117 @@
-import { useState, useEffect } from "react";
+import { languageGuesser } from "./utils/detectLanguage";
+
+const TWEET_LIMIT = 140;
+
+const TOMATO_VARIATIONS = {
+  en: "#tomato",
+  fr: "#tomate",
+  nl: "#tomaat",
+  it: "#pomodoro",
+  pl: "#pomidor",
+};
+
+function buildTomatoRegexp() {
+  const matcher = Object.values(TOMATO_VARIATIONS).reduce(
+    (acc, it, currentIndex) => {
+      if (currentIndex === 0) return `${it}`;
+      return `${acc}|${it}`;
+    },
+    ""
+  );
+
+  return new RegExp(matcher, "i");
+}
+
+function getLanguage(tweet) {
+  let language = "en";
+  const tomatoRegexp = buildTomatoRegexp();
+
+  const matchedHashtag = tweet.match(tomatoRegexp);
+
+  if (matchedHashtag) {
+    const matchedHashtagValue = matchedHashtag[0].toLowerCase();
+    const variation =
+      Object.values(TOMATO_VARIATIONS).indexOf(matchedHashtagValue);
+
+    return Object.keys(TOMATO_VARIATIONS)[variation];
+  }
+
+  languageGuesser.info(tweet, (info) => {
+    console.log(info);
+    const match = Object.keys(TOMATO_VARIATIONS).some((lg) => lg === info[0]);
+    if (match) {
+      language = info[0];
+    }
+  });
+
+  return language;
+}
+
+function getIsOnBrand(tweet, regexp) {
+  return regexp.test(tweet);
+}
+
+function onBrandProcess({ tweet, regexp, keyword, isWithinLimit }) {
+  const keywordPosition = tweet.search(regexp);
+  let processedTweet = "";
+  const isKeywordOverflowing = keywordPosition >= TWEET_LIMIT - keyword.length;
+
+  /*
+    POSSIBLE CASES FOR ON BRAND PROCESSING:
+    1 - THE TWEET IS WITHIN THE TWEET LIMIT 
+      * ACTION : DO NOTHING
+    2 - THE TWEET EXCEEDS THE LIMIT:
+      2.1 - THE KEYWORD OVERFLOWS THE TWEET LIMIT (IS PRESENT AFTER OR BEGINS "CROSSES" THE CHARACTER LIMIT) 
+        * ACTION: TRUNCATE THE TWEET TO ACCOMODATE THE KEYWORD AT THE END
+      2.2 - THE KEYWORD DOESN'T OVERFLOW THE TWEET LIMIT
+        * ACTION: TRUNCATE THE TWEET AT THE POSSIBLE LIMIT
+  */
+  if (isWithinLimit) {
+    return tweet;
+  }
+
+  if (isKeywordOverflowing) {
+    processedTweet =
+      // +1 to account for space
+      tweet.slice(0, TWEET_LIMIT - (keyword.length + 1)) + ` ${keyword}`;
+    return processedTweet;
+  }
+
+  return tweet.slice(0, TWEET_LIMIT);
+}
+
+function offBrandProcess({ tweet, language, isWithinLimit, keyword }) {
+  let processedTweet = tweet;
+  if (isWithinLimit) {
+    processedTweet += ` ${TOMATO_VARIATIONS[language]}`;
+    return processedTweet;
+  }
+
+  processedTweet = `${tweet.substr(0, 132).trim()} ${keyword}`;
+  return processedTweet;
+}
 
 async function addHashtag(tweet) {
-  let processedTweet = tweet;
+  const language = getLanguage(tweet);
+  const regexp = new RegExp(`${TOMATO_VARIATIONS[language]}`, "i");
+  const isOnBrand = getIsOnBrand(tweet, regexp);
+  const keyword = TOMATO_VARIATIONS[language];
+  const isWithinLimit = tweet.length <= TWEET_LIMIT;
 
-  if (!/#Tomato/i.test(tweet)) {
-    if (tweet.length >= 132) {
-      processedTweet = tweet.substr(0, 132).trim();
-    }
-    processedTweet += " #Tomato";
+  const processingPayload = {
+    tweet,
+    language,
+    regexp,
+    isOnBrand,
+    keyword,
+    isWithinLimit,
+  };
+
+  if (isOnBrand) {
+    return onBrandProcess(processingPayload);
   }
-  setTimeout(() => {}, 500);
 
-  return processedTweet;
+  return offBrandProcess(processingPayload);
 }
 
 function randomizeDelay() {
